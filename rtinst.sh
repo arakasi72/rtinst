93e9b33c280d855c5e76e64be887b34b5ee809d7
arakasi72 <arakasi72@users.noreply.github.com>
2014-10-14 19:22:55 +0100

    Update rtinst.sh

--- rtinst.sh
+++ rtinst.sh
@@ -12,6 +12,20 @@ DLFLAG=1
 logfile="/dev/null"
 gotip=0
 install_rt=0
+sshport=''
+
+#exit on error function
+error_exit() {
+echo "Error: $1"
+echo "This is most likely a network error, if your network is working, then it is likely a temporary issue with the relevant file server"
+echo "Run 'bash rtinst.sh -l' for detailed output to rtinst.log"
+echo "Once issue is resolved the script can be run again to complete installation"
+if ! [ -z "$sshport" ]; then
+  echo "SSH Port was set before script was stopped to $sshport"
+  echo "make sure you can login before closing this session"
+fi
+exit 1
+}
 
 #function to generate random password
 genpasswd() {
@@ -43,9 +57,7 @@ while [ $script_size = 0 ]
     rm -f $script_name
     attempts=$(( $attempts + 1 ))
     if [ $attempts = 20 ]; then
-      echo "There is a problem downloading the scripts. Please check your network or there may be an issue with the github website"
-      echo "If the Github website is down, you can try again later"
-      exit 1
+      error_exit "Problem downloading scripts from github - https://github.com/"
     fi
     wget --no-check-certificate https://raw.githubusercontent.com/arakasi72/rtinst/master/$script_name >> $logfile 2>&1
     script_size=$(du -b $script_name | cut -f1)
@@ -68,9 +80,7 @@ if [ $(dpkg-query -W -f='${Status}' $pack_name 2>/dev/null | grep -c "ok install
   if [ $? = 0 ]; then
     return 0
   else
-    echo "error trying to install $pack_name"
-    echo "Try to install using 'sudo apt-get install $pack_name' from command line"
-    echo "Then rerun rtinst.sh script"
+    error_exit "error trying to install $pack_name"
     exit
   fi
 else
@@ -210,27 +220,25 @@ if [ "$FULLREL" = "Ubuntu 12.04.5 LTS" ]; then
     cp /etc/apt/sources.list /etc/apt/sources.list.bak
     mv sources.list /etc/apt/sources.list
   else
-    echo "Unable to get sources file. This is most likely a network issue. 'bash rtinst.sh -l' to generate log and get more info."
-    exit 1
+    error_exit "Unable to download sources file from https://help.ubuntu.com/12.04/sample/sources.list"
   fi
 fi
 
-echo "Updating package lists"
-apt-get update > $logfile 2>&1
+echo "Updating package lists" | tee $logfile
+apt-get update >> $logfile 2>&1
 if ! [ $? = 0 ]; then
-  echo "Problem updating packages. This is most likely a network issue. 'bash rtinst.sh -l' to generate log and get more info."
-  exit 1
+  error_exit "Problem updating packages."
 fi
-echo "Upgrading packages"
+echo "Upgrading packages" | tee -a $logfile
 apt-get -y upgrade >> $logfile 2>&1
 if ! [ $? = 0 ]; then
-  echo "Problem updating packages. This is most likely a network issue. 'bash rtinst.sh -l' to generate log and get more info."
+  error_exit "Problem upgrading packages."
   exit 1
 fi
 apt-get clean && apt-get autoclean >> $logfile 2>&1
 
 #install the packsges needed
-echo "Installing required packages"
+echo "Installing required packages" | tee -a $logfile
 install_package sudo
 install_package nano
 install_package autoconf
@@ -290,8 +298,8 @@ install_package libjson-rpc-perl
 install_package libarchive-zip-perl
 
 if [ $RELNO = 14 ]; then
-  apt-add-repository -y ppa:jon-severinsson/ffmpeg >> $logfile 2>&1
-  apt-get update >> $logfile 2>&1
+  apt-add-repository -y ppa:jon-severinsson/ffmpeg >> $logfile 2>&1 || error_exit "Problem adding to repository from - https://launchpad.net/~jon-severinsson/+archive/ubuntu/ffmpeg"
+  apt-get update >> $logfile 2>&1 || error_exit "problem updating package lists"
 fi
 install_package ffmpeg
 
@@ -305,8 +313,8 @@ else
 fi
 
 # download rt scripts and config files
-echo "Fetching rtinst scripts"
-mkdir $home/rtscripts
+echo "Fetching rtinst scripts" | tee -a $logfile
+mkdir -p $home/rtscripts
 cd $home/rtscripts
 
 get_scripts rt /usr/local/bin/rt
@@ -328,7 +336,7 @@ get_scripts nginxsite
 cd $home
 
 # secure ssh
-echo "Securing SSH"
+echo "Securing SSH" | tee -a $logfile
 
 portline=$(grep 'Port ' /etc/ssh/sshd_config)
 if [ "$portline" = "Port 22" ]; then
@@ -368,7 +376,7 @@ sshport=$(grep 'Port ' /etc/ssh/sshd_config | sed 's/[^0-9]*//g')
 echo "SSH secured. Port set to $sshport"
 
 # install ftp
-echo "Installing vsftpd"
+echo "Installing vsftpd" | tee -a $logfile
 ftpport=$(random 41005 48995)
 
 if [ $RELNO = 12 ]; then
@@ -378,13 +386,13 @@ fi
 
 if [ $RELNO = 7 ]; then
   echo "deb http://ftp.cyconet.org/debian wheezy-updates main non-free contrib" | tee -a /etc/apt/sources.list.d/wheezy-updates.cyconet2.list > /dev/null
-  aptitude update  >> $logfile 2>&1
-  aptitude -o Aptitude::Cmdline::ignore-trust-violations=true -y install -t wheezy-updates debian-cyconet-archive-keyring vsftpd  >> $logfile 2>&1
+  aptitude update  >> $logfile 2>&1 || error_exit "problem updating package lists"
+  aptitude -o Aptitude::Cmdline::ignore-trust-violations=true -y install -t wheezy-updates debian-cyconet-archive-keyring vsftpd  >> $logfile 2>&1 || error_exit "Unable to download vsftpd"
 else
   install_package vsftpd
 fi
 
-echo "Configuring vsftpd"
+echo "Configuring vsftpd" | tee -a $logfile
 
 perl -pi -e "s/anonymous_enable=YES/anonymous_enable=NO/g" /etc/vsftpd.conf
 perl -pi -e "s/#local_enable=YES/local_enable=YES/g" /etc/vsftpd.conf
@@ -442,48 +450,45 @@ if [ $install_rt = 0 ]; then
   cd $home
   mkdir -p source
   cd source
-  echo "Downloading rtorrent source files"
+  echo "Downloading rtorrent source files" | tee -a $logfile
 
   if ! (svn co https://svn.code.sf.net/p/xmlrpc-c/code/stable xmlrpc  >> $logfile 2>&1); then
-    echo "Unable to download xmlrpc source files"
-    exit
+    error_exit "Unable to download xmlrpc source files from https://svn.code.sf.net/p/xmlrpc-c/code/stable"
   fi
 
   if ! (curl -# http://libtorrent.rakshasa.no/downloads/libtorrent-0.13.4.tar.gz | tar xz  >> $logfile 2>&1); then
-    echo "Unable to download libtorrent source files"
-    exit
+    error_exit "Unable to download libtorrent source files from http://libtorrent.rakshasa.no/downloads"
   fi
 
   if ! (curl -# http://libtorrent.rakshasa.no/downloads/rtorrent-0.9.4.tar.gz | tar xz  >> $logfile 2>&1); then
-    echo "Unable to download rtorrent source files"
-    exit
+    error_exit "Unable to download rtorrent source files from http://libtorrent.rakshasa.no/downloads"
   fi
 
   cd xmlrpc
-  echo "Installing xmlrpc"
+  echo "Installing xmlrpc" | tee -a $logfile
   ./configure --prefix=/usr --enable-libxml2-backend --disable-libwww-client --disable-wininet-client --disable-abyss-server --disable-cgi-server >> $logfile 2>&1
   make >> $logfile 2>&1
   make install >> $logfile 2>&1
 
   cd ../libtorrent-0.13.4
-  echo "Installing libtorrent"
+  echo "Installing libtorrent" | tee -a $logfile
   ./autogen.sh >> $logfile 2>&1
   ./configure --prefix=/usr >> $logfile 2>&1
   make -j2 >> $logfile 2>&1
   make install >> $logfile 2>&1
 
   cd ../rtorrent-0.9.4
-  echo "Installing rtorrent"
+  echo "Installing rtorrent" | tee -a $logfile
   ./autogen.sh >> $logfile 2>&1
   ./configure --prefix=/usr --with-xmlrpc-c >> $logfile 2>&1
   make -j2 >> $logfile 2>&1
   make install >> $logfile 2>&1
   ldconfig >> $logfile 2>&1
 else
- echo "skiping rtorrent installation"
+ echo "skiping rtorrent installation" | tee -a $logfile
 fi
 
-echo "Configuring rtorrent"
+echo "Configuring rtorrent" | tee -a $logfile
 cd $home
 
 mkdir -p rtorrent/.session
@@ -495,7 +500,7 @@ mv -f $home/rtscripts/.rtorrent.rc $home/.rtorrent.rc
 perl -pi -e "s/<user name>/$user/g" $home/.rtorrent.rc
 
 # install rutorrent
-echo "Installing Rutorrent"
+echo "Installing Rutorrent" | tee -a $logfile
 
 mkdir -p /var/www
 cd /var/www
@@ -504,12 +509,12 @@ if [ -d "/var/www/rutorrent" ]; then
   rm -r /var/www/rutorrent
 fi
 
-svn checkout http://rutorrent.googlecode.com/svn/trunk/rutorrent >> $logfile 2>&1
-svn checkout http://rutorrent.googlecode.com/svn/trunk/plugins >> $logfile 2>&1
+svn checkout http://rutorrent.googlecode.com/svn/trunk/rutorrent >> $logfile 2>&1 || error_exit "Unable to download rutorrent files from http://rutorrent.googlecode.com/svn/trunk/rutorrent"
+svn checkout http://rutorrent.googlecode.com/svn/trunk/plugins >> $logfile 2>&1 || error_exit "Unable to download rutorrent plugin files from http://rutorrent.googlecode.com/svn/trunk/plugins"
 rm -r rutorrent/plugins
 mv plugins rutorrent
 
-echo "Configuring Rutorrent"
+echo "Configuring Rutorrent" | tee -a $logfile
 rm rutorrent/conf/config.php
 mv $home/rtscripts/ru.config /var/www/rutorrent/conf/config.php
 mkdir -p /var/www/rutorrent/conf/users/$user/plugins
@@ -527,7 +532,7 @@ mv $home/rtscripts/ru.ini /var/www/rutorrent/plugins/conf/plugins.ini
 
 # install nginx
 cd $home
-echo "Installing nginx"
+echo "Installing nginx" | tee -a $logfile
 WEBPASS=$(genpasswd)
 htpasswd -c -b /var/www/rutorrent/.htpasswd $user $WEBPASS >> $logfile 2>&1
 
@@ -567,13 +572,13 @@ fi
 service nginx restart && service php5-fpm restart
 
 # install autodl-irssi
-echo "Installing autodl-irssi"
+echo "Installing autodl-irssi" | tee -a $logfile
 adlport=$(random 36001 36100)
 adlpass=$(genpasswd $(random 12 16))
 
 mkdir -p $home/.irssi/scripts/autorun
 cd $home/.irssi/scripts
-wget --no-check-certificate -O autodl-irssi.zip http://update.autodl-community.com/autodl-irssi-community.zip >> $logfile 2>&1
+wget --no-check-certificate -O autodl-irssi.zip http://update.autodl-community.com/autodl-irssi-community.zip >> $logfile 2>&1 || error_exit "Unable to download autodl scripts from http://update.autodl-community.com/"
 unzip -o autodl-irssi.zip >> $logfile 2>&1
 rm autodl-irssi.zip
 cp autodl-irssi.pl autorun/
@@ -581,7 +586,7 @@ mkdir -p $home/.autodl
 touch $home/.autodl/autodl.cfg && touch $home/.autodl/autodl2.cfg
 
 cd /var/www/rutorrent/plugins
-git clone https://github.com/autodl-community/autodl-rutorrent.git autodl-irssi >> $logfile 2>&1
+git clone https://github.com/autodl-community/autodl-rutorrent.git autodl-irssi >> $logfile 2>&1 || error_exit "Unable to download autodl plugin files from https://github.com/autodl-community/autodl-irssi"
 
 mkdir /var/www/rutorrent/conf/users/$user/plugins/autodl-irssi
 
@@ -602,7 +607,7 @@ echo "gui-server-password = $adlpass" | tee -a autodl2.cfg > /dev/null
 perl -pi -e "s/if \(\\$\.browser\.msie\)/if \(navigator\.appName \=\= \'Microsoft Internet Explorer\' \&\& navigator\.userAgent\.match\(\/msie 6\/i\)\)/g" /var/www/rutorrent/plugins/autodl-irssi/AutodlFilesDownloader.js
 
 # set permissions
-echo "Setting permissions, Starting services"
+echo "Setting permissions, Starting services" | tee -a $logfile
 chown -R www-data:www-data /var/www
 chmod -R 755 /var/www/rutorrent
 chown -R $user:$user $home
