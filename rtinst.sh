ff033446e823d39886866fe62618361b7d4f16cb
arakasi72 <arakasi72@users.noreply.github.com>
2016-05-05 00:20:51 +0100

    Update rtinst.sh

--- rtinst.sh
+++ rtinst.sh
@@ -16,7 +16,7 @@ rtorrentloc='http://rtorrent.net/downloads/rtorrent-'$rtorrentrel'.tar.gz'
 libtorrentloc='http://rtorrent.net/downloads/libtorrent-'$libtorrentrel'.tar.gz'
 xmlrpcloc='https://svn.code.sf.net/p/xmlrpc-c/code/stable'
 
-blob=develop
+blob=master
 rtdir=https://raw.githubusercontent.com/arakasi72/rtinst/$blob/scripts
 
 if [ -f /etc/lsb-release ]; then
