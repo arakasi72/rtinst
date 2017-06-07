5440ad0284fc2c29b2eb4998533371e9d2e2bd8d
windscolorme <windscolorme@users.noreply.github.com>
2017-06-07 17:06:53 +0800

    fix

--- rtinst.sh
+++ rtinst.sh
@@ -563,8 +563,8 @@ if [ $install_rt = 0 ]; then
   echo "Downloading rtorrent source files" | tee -a $logfile
 
   svn co $xmlrpcloc xmlrpc  >> $logfile 2>&1 || error_exit "Unable to download xmlrpc source files from https://svn.code.sf.net/p/xmlrpc-c/code/stable"
-  git clone https://github.com/rakshasa/libtorrent.git | tar xz  >> $logfile 2>&1 || error_exit "Unable to download libtorrent source files from http://libtorrent.rakshasa.no/downloads"
-  git clone https://github.com/rakshasa/rtorrent.git | tar xz  >> $logfile 2>&1 || error_exit "Unable to download rtorrent source files from http://libtorrent.rakshasa.no/downloads"
+  git clone https://github.com/rakshasa/libtorrent.git   >> $logfile 2>&1 || error_exit "Unable to download libtorrent source files from http://libtorrent.rakshasa.no/downloads"
+  git clone https://github.com/rakshasa/rtorrent.git   >> $logfile 2>&1 || error_exit "Unable to download rtorrent source files from http://libtorrent.rakshasa.no/downloads"
 
   cd xmlrpc
   echo "Installing xmlrpc" | tee -a $logfile
