e8de315b03b7217cbcc7b6d6b80b004dba1e551a
arakasi72 <arakasi72@users.noreply.github.com>
2017-06-12 12:21:24 +0100

    Create rtinst.sh

--- rtinst.sh
+++ rtinst.sh
@@ -565,7 +565,7 @@ if [ $install_rt = 0 ]; then
   echo "Downloading rtorrent source files" | tee -a $logfile
 
   svn co $xmlrpcloc xmlrpc  >> $logfile 2>&1 || error_exit "Unable to download xmlrpc source files from https://svn.code.sf.net/p/xmlrpc-c/code/stable"
-  
+
   if [ $relno -eq 9 ]; then
     git clone -q -b feature-bind --single-branch https://github.com/rakshasa/libtorrent.git libtorrent-$libtorrentrel
     git clone -q -b feature-bind --single-branch https://github.com/rakshasa/rtorrent.git rtorrent-$rtorrentrel
@@ -573,7 +573,7 @@ if [ $install_rt = 0 ]; then
     curl -# $libtorrentloc | tar xz  >> $logfile 2>&1 || error_exit "Unable to download libtorrent source files from http://libtorrent.rakshasa.no/downloads"
     curl -# $rtorrentloc | tar xz  >> $logfile 2>&1 || error_exit "Unable to download rtorrent source files from http://libtorrent.rakshasa.no/downloads"
   fi
-  
+
   cd xmlrpc
   echo "Installing xmlrpc" | tee -a $logfile
   ./configure --prefix=/usr --disable-cplusplus >> $logfile 2>&1
